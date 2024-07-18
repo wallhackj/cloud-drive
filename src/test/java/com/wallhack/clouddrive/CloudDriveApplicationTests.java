9b1987d3a54ba7251ad181096a30ca13411e2f16
wallhack <victorhaideu@gmail.com>
2024-07-18 09:57:36 +0300

    start

---
+++ src/test/java/com/wallhack/clouddrive/CloudDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallhack.clouddrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
